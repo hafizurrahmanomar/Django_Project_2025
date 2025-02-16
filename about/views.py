820264bd2cf886759c24297b391897d577fc275f
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2025-02-16 12:58:42 +0600

    Template

--- about/views.py
+++ about/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def about(request):
-    return render(request, 'about.html')
+    context={
+         "Name":"Hafizur Rahman",
+         "title":"I am a DevOps Engineer"
+    }   
+   
+    return render(request, 'about.html',context)
