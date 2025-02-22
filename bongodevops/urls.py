1849d7576a2688e66e93056fc1fbea9428cefe4e
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2025-02-22 10:08:48 +0600

    templateInheritance

--- bongodevops/urls.py
+++ bongodevops/urls.py
@@ -3,9 +3,18 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
+    path('product/', include('product.urls')),
 ]
+
+    
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
